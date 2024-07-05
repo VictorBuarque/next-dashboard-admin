31fa2ce699ae91e15377e69f87c03cfeb80f1c65
Victor Buarque <vcgabriel38@gmail.com>
2024-07-04 23:02:18 -0300

    feat: create side bar data

---
+++ src/data/sideBarData.tsx
@@ -0,0 +1,86 @@
+
+import { MdDashboard } from "react-icons/md";
+
+export type iSideBarMenu = {
+  title: string;
+  href: string;
+  icon: JSX.Element;
+};
+
+export const sideBarMenu = [
+  {
+    title: "Pages",
+    list: [
+      {
+        title: "Dashboard",
+        href: "/dashboard",
+        icon: <MdDashboard />,
+      },
+      {
+        title: "Contatos",
+        href: "/dashboard/contacts",
+        icon: <MdDashboard />,
+      },
+      {
+        title: "Clientes",
+        href: "/dashboard/clients",
+        icon: <MdDashboard />,
+      },
+      {
+        title: "Redes",
+        href: "/dashboard/store-network",
+        icon: <MdDashboard />,
+      },
+      {
+        title: "Lojas",
+        href: "/dashboard/stores",
+        icon: <MdDashboard />,
+      },
+      {
+        title: "Terminais",
+        href: "/dashboard/terminals",
+        icon: <MdDashboard />,
+      },
+      {
+        title: "Conteúdos",
+        href: "/dashboard/store-network",
+        icon: <MdDashboard />,
+      },
+      {
+        title: "Playlists",
+        href: "/dashboard/playlists",
+        icon: <MdDashboard />,
+      },
+      {
+        title: "Agendamentos",
+        href: "/dashboard/schedules",
+        icon: <MdDashboard />,
+      },
+      {
+        title: "Usuários",
+        href: "/dashboard/users",
+        icon: <MdDashboard />,
+      },
+    ],
+  },
+  {
+    title: "Analytics",
+    list: [
+      {
+        title: "Reports",
+        href: "/dashboard/reports",
+        icon: <MdDashboard />,
+      },
+    ],
+  },
+  {
+    title: "User",
+    list: [
+      {
+        title: "Logout",
+        href: "/logout",
+        icon: <MdDashboard />,
+      },
+    ],
+  },
+];
