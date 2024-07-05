3f13069aaf8d84f42a1d5090fba33921a05b1ac5
Victor Buarque <vcgabriel38@gmail.com>
2024-07-05 19:23:02 -0300

    feat: wip dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import Card from "@/components/dashboard/card/Card";
+import LastUpdates from "@/components/dashboard/last-updates/Last-Updates";
+import React from "react";
 
 export default function DashboardPage() {
   return (
-    <div>page</div>
-  )
+    <div>
+      <Card />
+      <LastUpdates />
+    </div>
+  );
 }
