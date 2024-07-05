de118b91a209cb5a3c1706415c1cf382edd6b3a2
Victor Buarque <vcgabriel38@gmail.com>
2024-07-04 23:02:09 -0300

    feat: create side bar

---
+++ src/components/dashboard/sidebar/SideBar.tsx
@@ -0,0 +1,60 @@
+import Link from "next/link";
+import { useState } from "react";
+import { MdAccountCircle, MdArrowDropDownCircle } from "react-icons/md";
+import { sideBarMenu } from "@/data/sideBarData";
+
+const SideBar = () => {
+  const [expandedCategories, setExpandedCategories] = useState<string[]>([]);
+
+  const toggleCategory = (title: string) => {
+    setExpandedCategories((prev) =>
+      prev.includes(title) ? prev.filter((cat) => cat !== title) : [...prev, title]
+    );
+  };
+
+  return (
+    <div>
+      <div className="flex items-center p-4 hover:bg-gray-100 dark:hover:bg-zinc-800/30">
+        <div className="w-5 h-5 rounded-full flex items-center justify-center">
+          <MdAccountCircle />
+        </div>
+        <span className="ml-4 font-semibold">Victor Gabriel</span>
+      </div>
+      {sideBarMenu.map((category) => (
+        <div key={category.title}>
+          <div
+            className="flex items-center justify-between p-4 hover:bg-gray-100 dark:hover:bg-zinc-800/30 cursor-pointer"
+            onClick={() => toggleCategory(category.title)}
+          >
+            <p className="text-sm font-semibold">{category.title}</p>
+            <div className="w-5 h-5 rounded-full flex items-center justify-center">
+              <MdArrowDropDownCircle
+                className={`transform transition-transform duration-300 ${
+                  expandedCategories.includes(category.title) ? 'rotate-180' : 'rotate-0'
+                }`}
+              />
+            </div>
+          </div>
+          {expandedCategories.includes(category.title) && (
+            <ul>
+              {category.list.map((item) => (
+                <li key={item.href}>
+                  <Link href={item.href}>
+                    <div className="flex items-center p-4 hover:bg-gray-100 dark:hover:bg-zinc-800/30">
+                      <div className="w-5 h-5 rounded-full flex items-center justify-center">
+                        {item.icon}
+                      </div>
+                      <span className="ml-4 font-semibold">{item.title}</span>
+                    </div>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default SideBar;
