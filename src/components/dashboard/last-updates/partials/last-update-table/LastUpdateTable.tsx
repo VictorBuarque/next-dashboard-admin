b7950d8cc8bbec05945be1a27681986942183746
Victor Buarque <vcgabriel38@gmail.com>
2024-07-05 19:55:47 -0300

    feat: create last update table

---
+++ src/components/dashboard/last-updates/partials/last-update-table/LastUpdateTable.tsx
@@ -0,0 +1,128 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+export default function LastUpdateTable() {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">Mod. Terminal</TableHead>
+          <TableHead>Rede</TableHead>
+          <TableHead>Nome Terminal</TableHead>
+          <TableHead>Tipo Terminal</TableHead>
+          <TableHead>Ult. Sinc</TableHead>
+          <TableHead>Data Att. Playlist</TableHead>
+          <TableHead>Status</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow className="uppercase">
+          <TableCell className="font-medium">PlayONN</TableCell>
+          <TableCell className="font-medium">CSP</TableCell>
+          <TableCell className="font-medium">csp_salvador_balcao_1</TableCell>
+          <TableCell className="font-medium">Horizontal</TableCell>
+          <TableCell className="font-medium">18/04/2023 18:16</TableCell>
+          <TableCell className="font-medium">03/06/2024 17:05</TableCell>
+          <TableCell className="font-medium">Desatualizado</TableCell>
+        </TableRow>
+        <TableRow className="uppercase">
+          <TableCell className="font-medium">PlayONN</TableCell>
+          <TableCell className="font-medium">CSP</TableCell>
+          <TableCell className="font-medium">csp_salvador_balcao_2</TableCell>
+          <TableCell className="font-medium">Horizontal</TableCell>
+          <TableCell className="font-medium">18/04/2023 18:16</TableCell>
+          <TableCell className="font-medium">03/06/2024 17:05</TableCell>
+          <TableCell className="font-medium">Desatualizado</TableCell>
+        </TableRow>
+        <TableRow className="uppercase">
+          <TableCell className="font-medium">PlayONN</TableCell>
+          <TableCell className="font-medium">CSP</TableCell>
+          <TableCell className="font-medium">csp_salvador_balcao_3</TableCell>
+          <TableCell className="font-medium">Horizontal</TableCell>
+          <TableCell className="font-medium">18/04/2023 18:16</TableCell>
+          <TableCell className="font-medium">03/06/2024 17:05</TableCell>
+          <TableCell className="font-medium">Atualizado</TableCell>
+        </TableRow>
+        <TableRow className="uppercase">
+          <TableCell className="font-medium">PlayONN</TableCell>
+          <TableCell className="font-medium">CSP</TableCell>
+          <TableCell className="font-medium">csp_salvador_balcao_4</TableCell>
+          <TableCell className="font-medium">Horizontal</TableCell>
+          <TableCell className="font-medium">18/04/2023 18:16</TableCell>
+          <TableCell className="font-medium">03/06/2024 17:05</TableCell>
+          <TableCell className="font-medium">Atualizado</TableCell>
+        </TableRow>
+        <TableRow className="uppercase">
+          <TableCell className="font-medium">PlayONN</TableCell>
+          <TableCell className="font-medium">CSP</TableCell>
+          <TableCell className="font-medium">csp_salvador_balcao_5</TableCell>
+          <TableCell className="font-medium">Horizontal</TableCell>
+          <TableCell className="font-medium">18/04/2023 18:16</TableCell>
+          <TableCell className="font-medium">03/06/2024 17:05</TableCell>
+          <TableCell className="font-medium">Atualizado</TableCell>
+        </TableRow>
+        {/* <TableRow className="uppercase">
+          <TableCell className="font-medium">PlayONN</TableCell>
+          <TableCell className="font-medium">CSP</TableCell>
+          <TableCell className="font-medium">csp_salvador_balcao_6</TableCell>
+          <TableCell className="font-medium">Horizontal</TableCell>
+          <TableCell className="font-medium">18/04/2023 18:16</TableCell>
+          <TableCell className="font-medium">03/06/2024 17:05</TableCell>
+          <TableCell className="font-medium">Atualizado</TableCell>
+        </TableRow>
+        <TableRow className="uppercase">
+          <TableCell className="font-medium">PlayONN</TableCell>
+          <TableCell className="font-medium">CSP</TableCell>
+          <TableCell className="font-medium">csp_salvador_balcao_7</TableCell>
+          <TableCell className="font-medium">Horizontal</TableCell>
+          <TableCell className="font-medium">18/04/2023 18:16</TableCell>
+          <TableCell className="font-medium">03/06/2024 17:05</TableCell>
+          <TableCell className="font-medium">Atualizado</TableCell>
+        </TableRow>
+        <TableRow className="uppercase">
+          <TableCell className="font-medium">PlayONN</TableCell>
+          <TableCell className="font-medium">CSP</TableCell>
+          <TableCell className="font-medium">csp_salvador_balcao_6</TableCell>
+          <TableCell className="font-medium">Horizontal</TableCell>
+          <TableCell className="font-medium">18/04/2023 18:16</TableCell>
+          <TableCell className="font-medium">03/06/2024 17:05</TableCell>
+          <TableCell className="font-medium">Atualizado</TableCell>
+        </TableRow>
+        <TableRow className="uppercase">
+          <TableCell className="font-medium">PlayONN</TableCell>
+          <TableCell className="font-medium">CSP</TableCell>
+          <TableCell className="font-medium">csp_salvador_balcao_8</TableCell>
+          <TableCell className="font-medium">Horizontal</TableCell>
+          <TableCell className="font-medium">18/04/2023 18:16</TableCell>
+          <TableCell className="font-medium">03/06/2024 17:05</TableCell>
+          <TableCell className="font-medium">Atualizado</TableCell>
+        </TableRow>
+        <TableRow className="uppercase">
+          <TableCell className="font-medium">PlayONN</TableCell>
+          <TableCell className="font-medium">CSP</TableCell>
+          <TableCell className="font-medium">csp_salvador_balcao_9</TableCell>
+          <TableCell className="font-medium">Horizontal</TableCell>
+          <TableCell className="font-medium">18/04/2023 18:16</TableCell>
+          <TableCell className="font-medium">03/06/2024 17:05</TableCell>
+          <TableCell className="font-medium">Atualizado</TableCell>
+        </TableRow>
+        <TableRow className="uppercase">
+          <TableCell className="font-medium">PlayONN</TableCell>
+          <TableCell className="font-medium">CSP</TableCell>
+          <TableCell className="font-medium">csp_salvador_balcao_10</TableCell>
+          <TableCell className="font-medium">Horizontal</TableCell>
+          <TableCell className="font-medium">18/04/2023 18:16</TableCell>
+          <TableCell className="font-medium">03/06/2024 17:05</TableCell>
+          <TableCell className="font-medium">Atualizado</TableCell>
+        </TableRow> */}
+      </TableBody>
+    </Table>
+  );
+}
