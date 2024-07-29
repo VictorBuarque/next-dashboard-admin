bded15996e81e72ddf2d3e72f8f60c5832828094
Victor Buarque <vcgabriel38@gmail.com>
2024-07-29 09:25:25 -0300

    feat: implement active item click

--- src/components/dashboard/sidebar/SideBar.tsx
+++ src/components/dashboard/sidebar/SideBar.tsx
@@ -7,6 +7,7 @@ import { sideBarMenu } from "@/data/sideBarData";
 
 const SideBar = () => {
   const [expandedCategories, setExpandedCategories] = useState<string[]>([]);
+  const [activeItem, setActiveItem] = useState<string | null>(null);
 
   const toggleCategory = (title: string) => {
     setExpandedCategories((prev) =>
@@ -16,17 +17,24 @@ const SideBar = () => {
     );
   };
 
+  const handleItemClick = (href: string) => {
+    setActiveItem(href);
+  };
+
   return (
-    <div className="h-[100%] flex flex-col justify-between rounded">
-      <div>
-        <div className="flex items-center p-4 h-16 hover:bg-gray-100 dark:hover:bg-zinc-800/30 self-center">
-          <MdAccountCircle size={20} />
-          <span className="ml-4 font-semibold">Victor Gabriel</span>
+    <div className="w-full h-[100%] flex flex-col justify-between rounded ">
+      <div className="mx-2">
+        <div className="flex items-center p-4 h-16 self-center">
+          <MdAccountCircle size={30} />
+          <div className="flex flex-col">
+            <span className="ml-4 font-semibold">Victor Gabriel</span>
+            <span className="ml-4 text-sm">Admin</span>
+          </div>
         </div>
         {sideBarMenu.map((category) => (
           <div key={category.title}>
             <div
-              className="flex items-center justify-between p-4 hover:bg-gray-100 dark:hover:bg-zinc-800/30 cursor-pointer"
+              className="flex items-center justify-between p-4 cursor-pointer hover:text-blue-500"
               onClick={() => toggleCategory(category.title)}
             >
               <p className="text-sm font-semibold">{category.title}</p>
@@ -44,13 +52,23 @@ const SideBar = () => {
             {expandedCategories.includes(category.title) && (
               <ul>
                 {category.list.map((item) => (
-                  <li key={item.href}>
+                  <li
+                    key={item.href}
+                    onClick={() => handleItemClick(item.href)}
+                    className={`cursor-pointer ${
+                      activeItem === item.href ? "bg-gray-50 text-blue-800 rounded  " : "hover:text-blue-500"
+                    }`}
+                  >
                     <Link href={item.href}>
-                      <div className="flex items-center p-4 hover:bg-gray-100 dark:hover:bg-zinc-800/30">
+                      <div className="flex items-center p-4">
                         <div className="w-5 h-5 rounded-full flex items-center justify-center">
                           {item.icon}
                         </div>
-                        <span className="ml-4 font-semibold">{item.title}</span>
+                        <span
+                          className="ml-4 font-semibold "
+                        >
+                          {item.title}
+                        </span>
                       </div>
                     </Link>
                   </li>
@@ -60,7 +78,7 @@ const SideBar = () => {
           </div>
         ))}
       </div>
-      <div className="block m-4">PlayOnn</div>
+      <div className="block m-4">Dev Victor Buarque</div>
     </div>
   );
 };
