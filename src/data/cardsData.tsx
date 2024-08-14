26e1062f0e98ec680c0294cfc0571ac4d4d4c9dc
Victor Buarque <vcgabriel38@gmail.com>
2024-08-14 06:14:06 -0300

    chore: text adjusts

--- src/data/cardsData.tsx
+++ src/data/cardsData.tsx
@@ -1,44 +1,54 @@
-import { MdOutlineSignalWifi4Bar, MdSignalWifiStatusbar2Bar, MdSignalWifiConnectedNoInternet0 } from "react-icons/md";
+import {
+  MdSupervisedUserCircle,
+} from "react-icons/md";
 
 export type iCardMenu = {
   title: string;
   count: number;
+  percentage: number;
   href: string;
+  message: string;
   icon: JSX.Element;
 };
 
 export const cardsMenu = [
-    {
-      title: "Nº de Terminais Atualizados",
-      list: [
-        {
-          count: 3000,
-          href: "/dashboard/terminals",
-          icon: <MdOutlineSignalWifi4Bar size={25} />,
-          subText: "Visualizar usuários"
-        },
-        ],
-    },
-    {
-      title: "Nº de Terminais Parcialmente Atualizados",
-      list: [
-        {
-          count: 2000,
-          href: "/dashboard/terminals",
-          icon: <MdSignalWifiStatusbar2Bar size={25} />,
-          subText: "Visualizar Terminais"
-        },
-        ],
-    },
-    {
-      title: "Nº de Terminais Desatualizados",
-      list: [
-        {
-          count: 1000,
-          href: "/dashboard/terminals",
-          icon: <MdSignalWifiConnectedNoInternet0 size={25} />,
-          subText: "Visualizar terminais"
-        },
-        ],
-    },
+  {
+    title: "Nº total de usuários",
+    list: [
+      {
+        count: 3000,
+        icon: <MdSupervisedUserCircle size={25} />,
+        subText: "Visualizar usuários",
+        href: "/dashboard/users",
+        percentage: 12,
+        message: "a mais do que semana passada",
+      },
+    ],
+  },
+  {
+    title: "Nº de Total de usuários",
+    list: [
+      {
+        count: 2000,
+        icon: <MdSupervisedUserCircle size={25} />,
+        subText: "Visualizar usuários",
+        href: "/dashboard/users",
+        percentage: 12,
+        message: "a mais do que semana passada",
+      },
+    ],
+  },
+  {
+    title: "Nº de Total de usuários",
+    list: [
+      {
+        count: 1000,
+        icon: <MdSupervisedUserCircle size={25} />,
+        subText: "Visualizar usuários",
+        href: "/dashboard/users",
+        percentage: 12,
+        message: "a mais do que semana passada",
+      },
+    ],
+  },
 ];
